375c839c1f865d281495c8443aeff0ec6eb90c91
Hao Lu <hl1319@ic.ac.uk>
2020-01-31 18:28:04 +0000

    update try files

--- try_sparse.cpp
+++ try_sparse.cpp
@@ -1,12 +1,9 @@
+
 #include <iostream>
 #include <math.h>
 #include <ctime>
-#include "Matrix.h"
-#include "Matrix.cpp"
 #include "solver.h"
 #include "solver.cpp"
-#include "CSRMatrix.h"
-#include "CSRMatrix.cpp"
 #include <vector>
 #include <memory>
 
@@ -14,7 +11,7 @@ using namespace std;
 
 int main()
 {
-    int rows(9), cols(9);
+    int rows(10), cols(10);
 
     /*int rp[6] = { 0,4,7,15,20,25 };
     int ci[25] = {0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4,0,1,2,3,4};
@@ -26,13 +23,18 @@ int main()
     int v[13] = {3,5,3,7,4,5,7,5,1,2,5,2,9};
     int nnzs = 13;*/
 
-    int rp[10] = { 0,3,6,10,15,20,23,27,30,31 };
-    int ci[31] = { 0,2,4,1,3,6,0,2,3,7,1,2,3,4,6,0,3,4,5,7,4,5,6,1,3,5,6,2,4,7,8 };
-    int v[31] = { 1,6,3,2,4,5,6,3,9,1,4,9,4,7,1,3,7,5,3,4,3,3,2,5,1,2,9,1,4,6,3 };
-    int nnzs = 31;
+//    int rp[10] = { 0,3,6,10,15,20,23,27,30,31 };
+//    int ci[31] = { 0,2,4,1,3,6,0,2,3,7,1,2,3,4,6,0,3,4,5,7,4,5,6,1,3,5,6,2,4,7,8 };
+//    int v[31] = { 1,6,3,2,4,5,6,3,9,1,4,9,4,7,1,3,7,5,3,4,3,3,2,5,1,2,9,1,4,6,3 };
+//    int nnzs = 31;
+    
+    int rp[11] = { 0,3,6,9,11,14,17,20,24,27,32 };
+    int ci[32] = { 0,2,7,1,6,9,0,2,5,3,6,4,7,9,2,5,8,1,3,6,0,4,7,9,5,8,9,1,4,7,8,9 };
+    double v[32] = { 6,3,3,8,4,1,3,9,2,7,3,9,5,4,2,8,6,4,3,6,3,5,7,2,6,9,1,1,4,2,1,8 };
+    int nnzs = 32;
 
-    int *row_position = new int[rows+1];
-    int *col_index = new int[nnzs];
+    auto *row_position = new int[rows+1];
+    auto *col_index = new int[nnzs];
     double *values = new double[nnzs];
 
     for (int i = 0; i < nnzs; i++)
@@ -50,13 +52,18 @@ int main()
 
     //cout << "-------row position assignment done-------" << endl;
     
-    //initialise x and b
+    // Sparse solvers tests
+    // 1. LU
+    // 2. Gauss-Seidel
+    // 3. Jacobi
+    
+    // initialising sparse_mat, b the RHS and x as pointers
     auto* sparse_mat = new CSRMatrix<double>(rows, cols, nnzs, values, row_position, col_index);
     cout << "Linear system: Matrix" << endl;
     sparse_mat->printMatrix();
-    sparse_mat->printDense();
+    //sparse_mat->printDense();
     
-    vector<double> barray = { 1, 2, -3, 8, 3, 6, 9, 4, 2 };
+    vector<double> barray = { 1, 2, -3, 8, 3, 6, 9, 4, 2, 3 };
     auto* b = new double[rows * 1];
     cout << "\nLinear system: RHS [";
     for (int i = 0; i < rows; i++)
@@ -67,8 +74,23 @@ int main()
     cout << "]" << endl;
 
     auto* x = new double[rows * 1];
+    
 
-    sparse_mat->LU_solver(b, x);
+    //********************** LU decomposition ***********************
+    // LU decomposition
+    //initialise x and b
+    //sparse_mat->LU_solver(b, x);
+    
+    int maxit = 10000;
+    double tol = 1e-4;
+    //*********************** Gauss-Seidel **************************
+    //gauss_seidel_sparse(*sparse_mat, x, b, maxit, tol);
+    
+    //*********************** Jacobi ********************************
+    jacobi_sparse(*sparse_mat, x, b, maxit, tol);
+    
+    
+    
 
     delete[] row_position;
     delete[] col_index;
