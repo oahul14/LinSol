bcf05fccd528961372576cf3a903f5c3aa52203f
Hao Lu <hl1319@ic.ac.uk>
2020-02-02 10:54:27 +0000

    LU solvers add comment

--- solver.h
+++ solver.h
@@ -11,9 +11,12 @@ using namespace std;
 
 template<class T>
 void LU_dense(Matrix<T>& A, T* x, T* b);
-
-template<class T>
-void LU_dense_blas(Matrix<T>& A, T* x, T* b);
+/*LU_dense method:
+ Created L and P for lower matrix and identity matrix to record pivotisation;
+ Pivotisation is done for better performance when dealing with non-dominant matrices;
+ Lower matrix could have been stored altogether with upper one, but for the consistency of the use of backward and forward substitution with other solvers it remains separate;
+ Calling the matVecMult, backward and forward substitution built in Matrix class;
+ */
 
 template<class T>
 void LU_sparse(CSRMatrix<T>& A, T* x, T* b);
@@ -25,16 +28,10 @@ template<class T>
 void gauss_seidel_dense(Matrix<T>& A, T* x, T* b, T er, T urf);
 
 template<class T>
-void gauss_seidel_dense_blas(Matrix<T>& A, T* x, T* b, T er, T urf);
-
-template<class T>
 void gauss_seidel_sparse(CSRMatrix<T>& A, T* x, T* b, int maxit, double tolerance);
 
 template<class T>
 void jacobi_dense(Matrix<T>& A, T* x, T* b, int maxit, double tolerance);
-
-template<class T>
-void jacobi_dense_blas(Matrix<T>& A, T* x, T* b, int maxit, double tolerance);
 
 template<class T>
 void jacobi_sparse(CSRMatrix<T>& A, T* x, T* b, int maxit, double tolerance);
@@ -50,3 +47,13 @@ void cholesky_dense(Matrix<T>& A, T* x, T* b);
 
 template<class T>
 void cholesky_sparse(Matrix<T>& A, T* x, T* b);
+
+template<class T>
+void LU_dense_blas(Matrix<T>& A, T* x, T* b);
+
+template<class T>
+void gauss_seidel_dense_blas(Matrix<T>& A, T* x, T* b, T er, T urf);
+
+template<class T>
+void jacobi_dense_blas(Matrix<T>& A, T* x, T* b, int maxit, double tolerance);
+
